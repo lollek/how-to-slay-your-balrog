0415d20b4b6c5e7a373b897b6ce29ccfd5c989b1
Olle K <olle.kvarnstroem@gmail.com>
2014-01-08 23:26:54 +0100

    Fixed compiler complaints and preprocessor stuff

--- src/wizard.c
+++ src/wizard.c
@@ -6,30 +6,14 @@
    not for profit purposes provided that this copyright and statement are
    included in all such copies. */
 
-#ifdef __TURBOC__
-#include	<stdio.h>
-#include	<stdlib.h>
-#endif /* __TURBOC__ */
- 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "config.h"
 #include "constant.h"
 #include "types.h"
 #include "externs.h"
-
-#ifdef USG
-#ifndef ATARIST_MWC
-#include <string.h>
-#endif
-#else
-#include <strings.h>
-#endif
-
-#ifdef ATARIST_TC
-/* Include this to get prototypes for standard library functions.  */
-#include <stdlib.h>
-#endif
-
-long atol();
 
 /* Light up the dungeon					-RAK-	*/
 void wizard_light()
